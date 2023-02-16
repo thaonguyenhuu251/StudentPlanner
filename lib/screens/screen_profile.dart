763e862e2992467f8491603016a2e18068b36262
NguyenHuuThao251 <nguyenhuuthao2001@gmail.com>
2023-02-16 11:42:51 +0700

    commit profile

--- lib/screens/screen_profile.dart
+++ lib/screens/screen_profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:studentplanner/presentation/schedule_icons.dart' as IconsCustom;
 
 class ProfileScreen extends StatelessWidget {
   @override
@@ -14,38 +15,109 @@ class ProfileScreen extends StatelessWidget {
             color: Colors.grey,
           ),
           Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
             decoration: BoxDecoration(color: Colors.white),
             child: Row(
-
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Column(
-                  children: [
-                    Text("БАКАЛАВРИАТСуворова Дарья Андреевна2068999-06-ИСТ-22-19"),
-                    Text("БАКАЛАВРИАТСуворова Дарья Андреевна2068999-06-ИСТ-22-19"),
-                    Text("БАКАЛАВРИАТСуворова Дарья Андреевна2068999-06-ИСТ-22-19"),
-                  ],
-                ),
-                Image.network(
-                  "https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg",
-                  height: 60,
-                  width: 60,
-                  fit: BoxFit.fill,
-                  loadingBuilder: (BuildContext context, Widget child,
-                      ImageChunkEvent? loadingProgress) {
-                    if (loadingProgress == null) return child;
-                    return Center(
-                      child: CircularProgressIndicator(
-                        value: loadingProgress.expectedTotalBytes != null
-                            ? loadingProgress.cumulativeBytesLoaded /
-                                loadingProgress.expectedTotalBytes!
-                            : null,
+                Expanded(
+                  flex: 5,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("БАКАЛАВРИАТС"),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 5),
+                        child: Text("БАКАЛАВРИАТС"),
                       ),
-                    );
-                  },
+                      Text("БАКАЛАВРИАТС"),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Image.network(
+                    "https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg",
+                    fit: BoxFit.fill,
+                    loadingBuilder: (BuildContext context, Widget child,
+                        ImageChunkEvent? loadingProgress) {
+                      if (loadingProgress == null) return child;
+                      return Center(
+                        child: CircularProgressIndicator(
+                          value: loadingProgress.expectedTotalBytes != null
+                              ? loadingProgress.cumulativeBytesLoaded /
+                                  loadingProgress.expectedTotalBytes!
+                              : null,
+                        ),
+                      );
+                    },
+                  ),
                 ),
               ],
             ),
-          )
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            decoration: BoxDecoration(color: Colors.white),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  selectedTileColor: Colors.blue,
+                  leading: Icon(IconsCustom.Schedule.mark),
+                  title: Align(
+                    child: Text(
+                      'Зачетная книжка',
+                      style: TextStyle(fontFamily: 'alkes', fontSize: 18),
+                    ),
+                  ),
+                  trailing: Icon(Icons.keyboard_arrow_right),
+                ),
+                Divider(
+                  height: 1,
+                  color: Colors.grey,
+                ),
+                ListTile(
+                  selectedTileColor: Colors.blue,
+                  leading: Icon(IconsCustom.Schedule.mark),
+                  title: Align(
+                    child: Text(
+                      'Зачетная книжка',
+                      style: TextStyle(fontFamily: 'alkes', fontSize: 18),
+                    ),
+                  ),
+                  trailing: Icon(Icons.keyboard_arrow_right),
+                ),
+                ListTile(
+                  selectedTileColor: Colors.blue,
+                  leading: Icon(IconsCustom.Schedule.mark),
+                  title: Align(
+                    child: Text(
+                      'Зачетная книжка',
+                      style: TextStyle(fontFamily: 'alkes', fontSize: 18),
+                    ),
+                  ),
+                  trailing: Icon(Icons.keyboard_arrow_right),
+                ),
+                ListTile(
+                  selectedTileColor: Colors.blue,
+                  leading: Icon(IconsCustom.Schedule.mark),
+                  title: Align(
+                    child: Text(
+                      'Зачетная книжка',
+                      style: TextStyle(fontFamily: 'alkes', fontSize: 18),
+                    ),
+                  ),
+                  trailing: Icon(Icons.keyboard_arrow_right),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
